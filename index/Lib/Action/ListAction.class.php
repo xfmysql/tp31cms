d10c2cc19b206f92ede2ab51c1ab1b0447c29cd9
xfmysql <xfmysql@qq.com>
2018-09-11 23:20:50 +0800

    article

--- index/Lib/Action/ListAction.class.php
+++ index/Lib/Action/ListAction.class.php
@@ -13,8 +13,9 @@ class ListAction extends CommonAction
 		$this->assign('set',$this->setting);
 		//数据分页
 		import('ORG.Util.Page');// 导入分页类
-   		$count=M('article')->where(" (pubtime is null or pubtime<CURRENT_TIMESTAMP() ) and pid in(select id from cms_catalog where id=$catsid or pid=$catsid) and status=1")->count();//获取数据的总
-   		$page=new Page($count,20);
+		$sql =" (pubtime is null or pubtime<CURRENT_TIMESTAMP() ) and pid in(select id from cms_catalog where id=$catsid or pid=$catsid) and status=1";
+   		$count=M('article')->where($sql)->count();//获取数据的总
+   		$page=new Page($count,16);
    		$page->setConfig('theme', '<li class="am-pagination-prev">%upPage%</li>
    			<li class="am-pagination-prev">%prePage%</li><li class="am-pagination-next">%nextPage%</li>
    			<li>%linkPage%</li><li class="am-pagination-next">%downPage%</li>
@@ -23,12 +24,12 @@ class ListAction extends CommonAction
 		
 		$page->setConfig('link',"/list-".$catsid."-".__PAGE__.".html");//分页变量名是p
 
-   		$show=$page->show();//返回分页信息 pubtime<CURRENT_TIMESTAMP()  and
-		$articles=M('article')->where(" (pubtime is null or pubtime<CURRENT_TIMESTAMP() ) and pid in(select id from cms_catalog where id=$catsid or pid=$catsid) and status=1")->
-		order('addtime desc')->limit($page->firstRow.','.$page->listRows)->select();
+   		$show=$page->show();//返回分页信息
+		$articles=M('article')->where($sql)->order('addtime desc')->limit($page->firstRow.','.$page->listRows)->select();
 		$this->assign('show',$show);
 		$this->assign('count',$count);
 		$this->assign('list',$articles);	
+		
 		
 		$article_mod = D('article');
 		//推荐文章 
